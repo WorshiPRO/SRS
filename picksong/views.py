402337beb0f0859d1e9d813dc97c21c60e729433
pingchihshih <b03902039@ntu.edu.tw>
2018-05-22 22:43:41 +0800

    try

--- picksong/views.py
+++ picksong/views.py
@@ -13,12 +13,9 @@ def inputsong(request):
 		newsong = Song()
 		newsong.name = request.POST.get("songname", "null")
 		newsong.owner = request.POST.get("band", "null")
-		if request.POST['key'] is not None:
-			newsong.key_of_song = request.POST.get("key", "n")
-		if request.POST['highest'] is not None:
-			newsong.highest_note = request.POST.get("highest", "n")
-		if request.POST['lowest'] is not None:
-			newsong.lowest_note = request.POST.get("lowest", "n")
+		newsong.key_of_song = request.POST.get("key", "n")
+		newsong.highest_note = request.POST.get("highest", "n")
+		newsong.lowest_note = request.POST.get("lowest", "n")
 		newsong.save()
 
 	return render(request, 'picksong.html', {'result': Song.objects.all()})
