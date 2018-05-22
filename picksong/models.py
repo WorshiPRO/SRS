4aaf03bfcea2035f8ca3a6dc2f62277d436325b6
Ubuntu <ubuntu@ip-172-31-13-122.us-west-1.compute.internal>
2018-05-22 07:51:24 +0000

    5/22 15:51 update all

---
+++ picksong/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+class Picksong(models.Model):
+	song = models.TextField(default="song")
+	owner = models.TextField(default="owner")
+	key_of_song = models.TextField(default="Bb")
+	submission_date = models.DateTimeField(auto_now=True)
+	
+	class Meta:
+		db_table = "songtest"
