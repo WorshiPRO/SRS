d77529e3a28d6627519451d84f13e190fab91b3b
pingchihshih <b03902039@ntu.edu.tw>
2018-05-23 18:07:26 +0800

    im dying

--- picksong/views.py
+++ picksong/views.py
@@ -1,20 +1,20 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
 from .models import Song
-from .forms import SongForm
+from .forms import ContactForm
 
 # Create your views here.
 def hello_view(request):
 	return render(request, 'hello.html', {'data': "Hello Django ",})
 
 def inputsong(request):
-	if request.method == 'POST':
-		print("hi!", request.POST)
-        form = SongForm(request.POST)
-        if form.is_valid():
-            pass  # does nothing, just trigger the validation
-	else:
-		form = SongForm()
+	# if request.method == 'POST':
+	# 	# print("hi!", request.POST)
+    #     form = SongForm(request.POST)
+    #     if form.is_valid():
+    #         pass  # does nothing, just trigger the validation
+	# else:
+	# 	form = SongForm()
 
 	# newsong = Song()
 	# newsong.name = request.POST.get("songname", "sad")
@@ -22,7 +22,12 @@ def inputsong(request):
 	# newsong.key_of_song = request.POST.get("key", "n")
 	# newsong.highest_note = request.POST.get("highest", "n")
 	# newsong.lowest_note = request.POST.get("lowest", "n")
-
+	if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            pass  # does nothing, just trigger the validation
+    else:
+        form = ContactForm()
 	arg = {'result': Song.objects.all(), 'form': form}
 	return render(request, 'picksong.html', arg)
 
