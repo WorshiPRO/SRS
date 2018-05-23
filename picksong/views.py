6c109b47b2c2be1966a8500181d7b4ba723c70b1
pingchihshih <b03902039@ntu.edu.tw>
2018-05-23 12:38:45 +0800

    update

--- picksong/views.py
+++ picksong/views.py
@@ -19,7 +19,8 @@ def inputsong(request):
 		newsong.key_of_song = request.POST.get("key", "n")
 		newsong.highest_note = request.POST.get("highest", "n")
 		newsong.lowest_note = request.POST.get("lowest", "n")
-		newsong.save()
+		if(songname is not "null")
+			newsong.save()
 
 	return redirect('/')
 	#return render(request, 'picksong.html', {'result': Song.objects.all()})
