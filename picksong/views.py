21727354981631a9473a1816842c75c9658714a5
pingchihshih <b03902039@ntu.edu.tw>
2018-05-23 16:47:42 +0800

    try sth dangerous

--- picksong/views.py
+++ picksong/views.py
@@ -16,8 +16,8 @@ def inputsong(request):
 		newsong.key_of_song = request.POST.get("key", "n")
 		newsong.highest_note = request.POST.get("highest", "n")
 		newsong.lowest_note = request.POST.get("lowest", "n")
-		#if newsong.name is not "null":
-		newsong.save()
+		#if newsong.name is not "sad":
+			newsong.save()
 
 	#return redirect('/')
 	return render(request, 'picksong.html', {'result': Song.objects.all()})
