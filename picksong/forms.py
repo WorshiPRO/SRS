f69b048ef70925e2ba88157127479ed30b794585
pingchihshih <b03902039@ntu.edu.tw>
2018-05-23 18:40:17 +0800

    transforming into django form

--- picksong/forms.py
+++ picksong/forms.py
@@ -2,8 +2,25 @@ from django import forms
 
 class SongForm(forms.Form):
     song = forms.CharField(max_length=30,
-        widget=forms.TextInput(attrs={'id': 'songname',
-                                        'class': 'form-control',
-                                        'placeholder':'輸入歌曲名'}))
-    #owner = forms.CharField(max_length=30)
-    #key = forms.CharField(max_length=30)
+        widget=forms.TextInput(attrs={'id': 'name',
+                                      'class': 'form-control',
+                                      'placeholder':'輸入歌曲名, 例如: 喜樂泉源'}))
+    owner = forms.CharField(max_length=30,
+        widget=forms.TextInput(attrs={'id': 'owner',
+                                      'class': 'form-control',
+                                      'placeholder':'輸入歌手/樂團/教會, 例如: 讚美之泉'}))
+
+    key = forms.CharField(max_length=30,
+        widget=forms.TextInput(attrs={'id': 'key',
+                                      'class': 'form-control',
+                                      'placeholder':'輸入歌曲key 例如: G'}))
+
+    highest = forms.CharField(max_length=30,
+        widget=forms.TextInput(attrs={'id': 'highet',
+                                      'class': 'form-control',
+                                      'placeholder':'輸入歌曲最高音 例如: C5(女))'}))
+
+    lowest = forms.CharField(max_length=30,
+        widget=forms.TextInput(attrs={'id': 'lowest',
+                                      'class': 'form-control',
+                                      'placeholder':'輸入歌曲最高音 例如: D4(女))'}))
