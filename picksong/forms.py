c7c3f72c4921a27fd288f39f55c51b8697637906
pingchihshih <b03902039@ntu.edu.tw>
2018-05-23 18:11:57 +0800

    dying

--- picksong/forms.py
+++ picksong/forms.py
@@ -2,7 +2,3 @@ from django import forms
 
 class SongForm(forms.Form):
     song = forms.CharField(max_length=30)
-	owner = forms.CharField(max_length=30)
-	key_of_song = forms.CharField(max_length=10)
-	highest_note = forms.CharField(max_length=10)
-	lowest_note = forms.CharField(max_length=10)
