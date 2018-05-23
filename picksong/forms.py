e94593eb64da70c395a4b326c5174ae6336ecba7
pingchihshih <b03902039@ntu.edu.tw>
2018-05-23 18:28:35 +0800

    dying

--- picksong/forms.py
+++ picksong/forms.py
@@ -1,5 +1,9 @@
 from django import forms
 
 class SongForm(forms.Form):
-    song = forms.CharField(max_length=30)
-    owner = forms.CharField(max_length=30)
+    song = forms.CharField(max_length=30,
+        widget=forms.TextInput(attrs={'id': 'songname',
+                                        'class': 'form-control',
+                                        'placeholder':'輸入歌曲名'}))
+    #owner = forms.CharField(max_length=30)
+    #key = forms.CharField(max_length=30)
