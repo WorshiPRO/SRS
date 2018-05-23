0131cf87b388f6f9c7413ca884ee1fa16f9f2c55
pingchihshih <b03902039@ntu.edu.tw>
2018-05-23 16:25:35 +0800

    try sth dangerous

--- picksong/views.py
+++ picksong/views.py
@@ -16,7 +16,7 @@ def inputsong(request):
 		newsong.key_of_song = request.POST.get("key", "n")
 		newsong.highest_note = request.POST.get("highest", "n")
 		newsong.lowest_note = request.POST.get("lowest", "n")
-		if(songname is not "null")
+		if songname is not "null"
 			newsong.save()
 
 	#return redirect('/')
