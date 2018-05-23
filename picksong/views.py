25f30d4f4ed92f670e3d9e66039dc97b088a8502
pingchihshih <b03902039@ntu.edu.tw>
2018-05-23 17:59:22 +0800

    disable csrf token

--- picksong/views.py
+++ picksong/views.py
@@ -1,28 +1,30 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
 from .models import Song
+from .forms import SongForm
 
 # Create your views here.
 def hello_view(request):
 	return render(request, 'hello.html', {'data': "Hello Django ",})
 
 def inputsong(request):
-	if request.method == 'GET':
-		#fo = open("viewslog.out", "w")
-		#fo.write(str(request.POST.keys()))
-		print("hi!")
-		print(request.GET)
-		newsong = Song()
-		newsong.name = request.GET.get("songname", "sad")
-		newsong.owner = request.GET.get("band", "too_sad")
-		newsong.key_of_song = request.GET.get("key", "n")
-		newsong.highest_note = request.GET.get("highest", "n")
-		newsong.lowest_note = request.GET.get("lowest", "n")
-		if newsong.name is not "sad":
-			newsong.save()
+	if request.method == 'POST':
+		print("hi!", request.POST)
 
-	#return redirect('/')
-	arg = {'result': Song.objects.all()}
+        form = SongForm(request.POST)
+        if form.is_valid():
+            pass  # does nothing, just trigger the validation
+	else:
+		form = SongForm()
+
+	# newsong = Song()
+	# newsong.name = request.POST.get("songname", "sad")
+	# newsong.owner = request.POST.get("band", "too_sad")
+	# newsong.key_of_song = request.POST.get("key", "n")
+	# newsong.highest_note = request.POST.get("highest", "n")
+	# newsong.lowest_note = request.POST.get("lowest", "n")
+
+	arg = {'result': Song.objects.all(), 'form': form}
 	return render(request, 'picksong.html', arg)
 
 def result(request):
