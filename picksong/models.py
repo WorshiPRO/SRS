b4a9862507fedac6edce44fedbd3d8dac027143b
pingchihshih <b03902039@ntu.edu.tw>
2018-05-25 22:01:12 +0800

    try to add new pages: searchsong & result

--- picksong/models.py
+++ picksong/models.py
@@ -22,18 +22,28 @@ class Song(models.Model):
 				"F", "F#",
 				"G", "G#", "Gb",]
 		if self.key_of_song not in valid_keys:
+			print("[Error] models.py->Song->is_valid: undefined key")
 			return False
 		elif len(self.highest_note) is 2:
+			print("[Status] models.py->Song->is_valid: len(highest_note)=2")
 			if self.highest_note[0] not in valid_keys or int(self.highest_note[1]) not in range(8):
+				print("[Error] models.py->Song->is_valid: highest_note not found")
 				return False
 		elif len(self.highest_note) is 3:
+			print("[Status] models.py->Song->is_valid: len(highest_note)=3")
 			if self.highest_note[:2] not in valid_keys or int(self.highest_note[2]) not in range(8):
+				print("[Error] models.py->Song->is_valid: highest_note not found")
 				return False
-		elif len(self.lowest_note) is 2:
+
+		if len(self.lowest_note) is 2:
+			print("[Status] models.py->Song->is_valid: len(lowest_note)=2")
 			if self.lowest_note[0] not in valid_keys or int(self.lowest_note[1]) not in range(8):
+				print("[Error] models.py->Song->is_valid: lowest_note not found")
 				return False
 		elif len(self.lowest_note) is 3:
+			print("[Status] models.py->Song->is_valid: len(lowest_note)=3")
 			if self.lowest_note[:2] not in valid_keys or int(self.lowest_note[2]) not in range(8):
+				print("[Error] models.py->Song->is_valid: lowest_note not found")
 				return False
 		else:
 			return True
