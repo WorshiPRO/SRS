48e2b79ae0f63ad436c3fbd771050fbf3db60994
pingchihshih <b03902039@ntu.edu.tw>
2018-05-25 12:44:52 +0800

    try to add new function: input validation

--- picksong/views.py
+++ picksong/views.py
@@ -19,12 +19,15 @@ def inputsong(request):
 			newsong.lowest_note = form.cleaned_data['lowest']
 			if newsong.is_valid():
 				newsong.save()
+				error_msg = ''
+			else:
+				error_msg = '輸入格式錯誤'
 			form = SongForm()
 	else: # get
 		form = SongForm()
 		error_msg = ''
 
-	error_msg = '輸入格式錯誤' if not newsong.is_valid() else ''
+
 
 	arg = {'result': Song.objects.all(), 'form': form, 'error_msg': error_msg}
 	return render(request, 'picksong.html', arg)
