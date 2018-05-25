6365cb663038130bf0e7d9cdfe62a00edebe8da5
pingchihshih <b03902039@ntu.edu.tw>
2018-05-25 22:58:56 +0800

    try to link to another page

--- SRS/urls.py
+++ SRS/urls.py
@@ -21,7 +21,7 @@ from picksong.views import searchsong
 from picksong.views import result
 
 urlpatterns = [
-	path('', inputsong, namespace="picksong"),
+	path('', inputsong, names="picksong"),
     path('admin/', admin.site.urls),
 	path('hello/', hello_view),
     path('inputsong/', inputsong),
