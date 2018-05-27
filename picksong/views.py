a5369861c556fc65799e1e140d21d29f367add2e
pingchihshih <b03902039@ntu.edu.tw>
2018-05-27 20:47:12 +0800

    add some debug msg in views.py

--- picksong/views.py
+++ picksong/views.py
@@ -48,13 +48,11 @@ def result(request):
 			if form.is_valid():
 				print("[Status] views.py: form is valid")
 				if form.cleaned_data['name'] != "":
-					print("[Status] views.py: name isn't empty")
 					if form.cleaned_data['name'] not in row.name:
 						print("[Status] views.py: name substr not found")
 						hit = False
 				if form.cleaned_data['owner'] != "":
-					print("[Status] views.py: owner isn't empty")
-					if form.cleaned_data['owner'] not in row.name:
+					if form.cleaned_data['owner'] not in row.owner:
 						print("[Status] views.py: owner substr not found")
 						hit = False
 				# if form.cleaned_data['key'] != "":
